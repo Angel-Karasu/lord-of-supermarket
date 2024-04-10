f76621179a152a6850fd1d6dd08a48d0ea73da27
Angel-Karasu <angel.karasu@proton.me>
2024-04-10 17:11:00 +0200

    Update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 
 HOST = '0.0.0.0'
-PORT = 5000
+PORT = 5500
 
 app = Flask(__name__)
 
@@ -11,4 +11,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host=HOST, port=PORT)
+    app.run(host=HOST, port=PORT, debug=True)

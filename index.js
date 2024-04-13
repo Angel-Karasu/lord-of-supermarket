013ee65dfdafcb31a8dc031efa729a55fbf8b4b0
Angel-Karasu <angel.karasu@proton.me>
2024-04-13 19:15:54 +0200

    Update

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const supermarket_scraper_api_url = 'http://localhost:5500'
 
-let search_input, sort, order;
+let search_input, sortby, descending_order;
 let page = 1;
 
-function search() {
-    window.location.hash = `search=${search_input.value}&sort=${sort.value}&order=${order.value}`;
+function search_product() {
+    let search = `search=${search_input.value}&page=1&sortby=${sortby.value}&descending_order=${descending_order.checked}&list_supermarket_id=0&list_supermarket_id=116`;
+    window.location.hash = search;
 
-    fetch(`${supermarket_scraper_api_url}/get_sortby_methods`, {
+    fetch(`${supermarket_scraper_api_url}/search_product?${search}`, {
         method: 'GET',
         headers: {'Content-Type': 'application/json'},
     }).then(response => response.json()).then(data => console.log(data));
@@ -14,9 +15,19 @@ function search() {
 
 window.onload = () => {
     search_input = document.querySelector('#search-input');
-    sort = document.querySelector('#sort');
-    order = document.querySelector('#order');
+    sortby = document.querySelector('#sortby');
+    descending_order = document.querySelector('#descending-order-checkbox');
 
-    search_input.onchange = search;
-    document.querySelector('#search-button').onclick = search;
+    fetch(`${supermarket_scraper_api_url}/get_sortby_methods`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    }).then(res => res.json()).then(sortby_methods => sortby_methods.forEach(method => {
+        let option = document.createElement('option');
+        option.value = method;
+        option.innerHTML = method.charAt(0).toUpperCase() + method.substr(1).replace('_', ' ');
+        sortby.appendChild(option);
+    }));
+
+    search_input.onchange = search_product;
+    document.querySelector('#search-button').onclick = search_product;
 }

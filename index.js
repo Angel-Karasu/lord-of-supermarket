84c0098adb936ae8311eb50eee18b143e3971c8f
Angel-Karasu <angel.karasu@proton.me>
2024-04-16 13:52:40 +0200

    Update

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const supermarket_scraper_api_url = 'http://localhost:5500'
+const supermarket_scraper_api_url = 'http://localhost:5500';
+let API_activated = true;
 
-let search_input, sortby, descending_order;
+const supermarkets = fetch(`${supermarket_scraper_api_url}/get_supermarkets`).then(res => {
+    if (!res.ok) throw new Error('API not activated');
+    return res.json();
+}).catch(err => {
+    alert(err);
+    API_activated = false;
+    return err;
+});
+
+let search_products, search_products_input, sortby, descending_order;
 let page = 1;
 
 function search_product() {
-    let search = `search=${search_input.value}&page=1&sortby=${sortby.value}&descending_order=${descending_order.checked}&list_supermarket_id=0&list_supermarket_id=116`;
+    let search = `search=${search_products_input.value}&page=${page}&sortby=${sortby.value}&descending_order=${descending_order.checked}&list_supermarket_id=0&list_supermarket_id=116`;
     window.location.hash = search;
 
-    fetch(`${supermarket_scraper_api_url}/search_product?${search}`, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'},
-    }).then(response => response.json()).then(data => console.log(data));
+    fetch(`${supermarket_scraper_api_url}/search_product?${search}`).then(res => res.json()).then(data => console.log(data));
 }
 
-window.onload = () => {
-    search_input = document.querySelector('#search-input');
-    sortby = document.querySelector('#sortby');
-    descending_order = document.querySelector('#descending-order-checkbox');
+window.onload = async () => {
+    search_products = document.querySelector('#search-products');
+    await supermarkets;
+    console.log(await supermarkets);
 
-    fetch(`${supermarket_scraper_api_url}/get_sortby_methods`, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'},
-    }).then(res => res.json()).then(sortby_methods => sortby_methods.forEach(method => {
+    if (!API_activated) {
+        document.querySelector('header').innerHTML = document.querySelector('header h1').outerHTML;
+        document.querySelector('main').innerHTML = `<span id="error">${await supermarkets}</span>`;
+        return;
+    }
+
+    search_products_input = search_products.querySelector('input');
+    sortby = search_products.querySelector('#sortby');
+    descending_order = search_products.querySelector('input[type="checkbox"]');
+
+    fetch(`${supermarket_scraper_api_url}/get_sortby_methods`).then(res => res.json()).then(sortby_methods => sortby_methods.forEach(method => {
         let option = document.createElement('option');
         option.value = method;
         option.innerHTML = method.charAt(0).toUpperCase() + method.substr(1).replace('_', ' ');
         sortby.appendChild(option);
     }));
 
-    search_input.onchange = search_product;
-    document.querySelector('#search-button').onclick = search_product;
+    search_products_input.onsearch = search_product;
+    search_products.querySelector('button').onclick = search_product;
 }
